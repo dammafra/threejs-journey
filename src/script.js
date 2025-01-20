9ea5d4cd5082617ef89f75d8b20b10fe2f0755e9
Francesco Dammacco <francescodammacco@live.it>
2025-01-20 23:32:34 +0100

    Final

--- src/script.js
+++ src/script.js
@@ -7,10 +7,59 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Objects
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 }),
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff }),
+)
+cube3.position.x = 2
+group.add(cube3)
+
+// Position
+// group.position.x = 0.7
+// group.position.y = -0.6
+// group.position.z = 1
+group.position.set(0.7, -0.6, 1)
+// make vector length 1
+// group.position.normalize()
+console.log('group position length: ', group.position.length())
+
+// Scale
+// group.scale.x = 2
+// group.scale.y = 1
+// group.scale.z = 0.5
+group.scale.set(2, 1, 0.5)
+
+// Rotation
+group.rotation.reorder('YXZ')
+group.rotation.x = Math.PI * 0.25
+group.rotation.y = Math.PI * 0.25
+
+//  Axes helper
+const axesLength = 5
+
+const axesHelper = new THREE.AxesHelper(axesLength)
+// axesHelper.position.set(group.position.x, group.position.y, group.position.z)
+scene.add(axesHelper)
 
 // Sizes
 const sizes = {
@@ -20,8 +69,14 @@ const sizes = {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.setZ(10)
 scene.add(camera)
+
+// Look at
+camera.lookAt(group.position)
+
+console.log('camera position length: ', camera.position.length())
+console.log('group distance to camera:', group.position.distanceTo(camera.position))
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({

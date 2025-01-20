e74d464764fdddec59cf04c6e97669333f98266e
Francesco Dammacco <francescodammacco@live.it>
2025-01-20 23:58:26 +0100

    Add tweak for texture selection

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 // Debug
 const gui = new GUI({ width: 300 })
 const debug = {
+  selectedTexture: 'door',
   rotationDegrees: 0,
   reset: () => gui.reset(),
 }
@@ -16,6 +17,12 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial()
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
 // Textures
 const loadingManager = new THREE.LoadingManager()
 // loadingManager.onStart = () => console.log('loading started')
@@ -24,51 +31,13 @@ const loadingManager = new THREE.LoadingManager()
 // loadingManager.onError = () => console.log('loading error')
 
 const textureLoader = new THREE.TextureLoader(loadingManager)
+const colorTextureMap = {
+  door: '/textures/door/color.jpg',
+  checkerboard1024: '/textures/checkerboard-1024x1024.png',
+  checkerboard8: '/textures/checkerboard-8x8.png',
+  minecraft: '/textures/minecraft.png',
+}
 // const colorTexture = textureLoader.load('/textures/door/color.jpg')
-// const colorTexture = textureLoader.load('/textures/checkerboard-1024x1024.png')
-// const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png')
-const colorTexture = textureLoader.load('/textures/minecraft.png')
-colorTexture.colorSpace = THREE.SRGBColorSpace
-
-const transformFolder = gui.addFolder('Transform')
-transformFolder.add(colorTexture.repeat, 'x').min(1).max(5).step(1).name('repeat x')
-transformFolder.add(colorTexture.repeat, 'y').min(1).max(5).step(1).name('repeat y')
-transformFolder.add(colorTexture, 'wrapS').options({
-  RepeatWrapping: THREE.RepeatWrapping,
-  ClampToEdgeWrapping: THREE.ClampToEdgeWrapping,
-  MirroredRepeatWrapping: THREE.MirroredRepeatWrapping,
-})
-transformFolder.add(colorTexture, 'wrapT').options({
-  RepeatWrapping: THREE.RepeatWrapping,
-  ClampToEdgeWrapping: THREE.ClampToEdgeWrapping,
-  MirroredRepeatWrapping: THREE.MirroredRepeatWrapping,
-})
-transformFolder.add(colorTexture.offset, 'x').min(-1).max(1).step(0.01).name('offset x')
-transformFolder.add(colorTexture.offset, 'y').min(-1).max(1).step(0.01).name('offset y')
-transformFolder
-  .add(debug, 'rotationDegrees')
-  .min(0)
-  .max(360)
-  .step(1)
-  .onChange(value => {
-    colorTexture.rotation = THREE.MathUtils.degToRad(value)
-  })
-transformFolder.add(colorTexture.center, 'x').min(0).max(1).step(0.01).name('center x')
-transformFolder.add(colorTexture.center, 'y').min(0).max(1).step(0.01).name('center y')
-
-const filteringFolder = gui.addFolder('Filtering')
-filteringFolder.add(colorTexture, 'generateMipmaps')
-filteringFolder.add(colorTexture, 'minFilter').options({
-  NearestFilter: THREE.NearestFilter,
-  Default: THREE.LinearMipmapLinearFilter,
-})
-filteringFolder.add(colorTexture, 'magFilter').options({
-  NearestFilter: THREE.NearestFilter,
-  Default: THREE.LinearFilter,
-})
-
-gui.onChange(() => (colorTexture.needsUpdate = true))
-
 // const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
 // const heightTexture = textureLoader.load('/textures/door/height.jpg')
 // const normalTexture = textureLoader.load('/textures/door/normal.jpg')
@@ -76,11 +45,9 @@ gui.onChange(() => (colorTexture.needsUpdate = true))
 // const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 // const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ map: colorTexture })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+gui.add(debug, 'selectedTexture').options(Object.keys(colorTextureMap)).onChange(loadTexture)
+
+loadTexture(debug.selectedTexture)
 
 // Sizes
 const sizes = {
@@ -103,10 +70,8 @@ window.addEventListener('resize', () => {
 })
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.set(1, 1, 2)
 scene.add(camera)
 
 // Controls
@@ -114,17 +79,65 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 // Renderer
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-})
+const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // Animate
 const animate = () => {
-  // Update controls
   controls.update()
-
   renderer.render(scene, camera)
 }
 renderer.setAnimationLoop(animate)
+
+// Helpers --------------------------------------------------------------------
+function loadTexture(textureKey) {
+  const texture = textureLoader.load(colorTextureMap[textureKey])
+  texture.colorSpace = THREE.SRGBColorSpace
+
+  mesh.material.dispose()
+  mesh.material = new THREE.MeshBasicMaterial({ map: texture })
+
+  destroyTextureGUI()
+  setupTextureGUI(texture)
+}
+
+function destroyTextureGUI() {
+  while (gui.folders.length) gui.folders[0].destroy()
+}
+
+function setupTextureGUI(texture) {
+  // Transform
+  const transformFolder = gui.addFolder('Transform')
+  transformFolder.add(texture.repeat, 'x').min(1).max(5).step(1).name('repeat x')
+  transformFolder.add(texture.repeat, 'y').min(1).max(5).step(1).name('repeat y')
+  transformFolder.add(texture, 'wrapS').options({
+    RepeatWrapping: THREE.RepeatWrapping,
+    ClampToEdgeWrapping: THREE.ClampToEdgeWrapping,
+    MirroredRepeatWrapping: THREE.MirroredRepeatWrapping,
+  })
+  transformFolder.add(texture, 'wrapT').options({
+    RepeatWrapping: THREE.RepeatWrapping,
+    ClampToEdgeWrapping: THREE.ClampToEdgeWrapping,
+    MirroredRepeatWrapping: THREE.MirroredRepeatWrapping,
+  })
+  transformFolder.add(texture.offset, 'x').min(-1).max(1).step(0.01).name('offset x')
+  transformFolder.add(texture.offset, 'y').min(-1).max(1).step(0.01).name('offset y')
+  transformFolder.add(debug, 'rotationDegrees').min(0).max(360).step(1).onChange(value => (texture.rotation = THREE.MathUtils.degToRad(value))).setValue(0) //prettier-ignore
+  transformFolder.add(texture.center, 'x').min(0).max(1).step(0.01).name('center x')
+  transformFolder.add(texture.center, 'y').min(0).max(1).step(0.01).name('center y')
+  transformFolder.onChange(() => (texture.needsUpdate = true))
+
+  // Filtering
+  const filteringFolder = gui.addFolder('Filtering')
+  filteringFolder.add(texture, 'generateMipmaps')
+  filteringFolder.add(texture, 'minFilter').options({
+    NearestFilter: THREE.NearestFilter,
+    Default: THREE.LinearMipmapLinearFilter,
+  })
+  filteringFolder.add(texture, 'magFilter').options({
+    NearestFilter: THREE.NearestFilter,
+    Default: THREE.LinearFilter,
+  })
+  filteringFolder.onChange(() => (texture.needsUpdate = true))
+}

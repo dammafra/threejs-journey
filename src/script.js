73cc8cfd6cb326630e8e2762db829ed8c446b64c
Francesco Dammacco <francescodammacco@live.it>
2025-01-21 00:14:57 +0100

    Final

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 import GUI from 'lil-gui'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
 
 // Debug
 const gui = new GUI()
@@ -12,14 +13,47 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Lights
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5)
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 50)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.9)
+directionalLight.position.set(1, 0.25, 0)
+scene.add(directionalLight)
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.9)
+scene.add(hemisphereLight)
+
+const pointLight = new THREE.PointLight(0xff9000, 1.5, 3, 2)
+pointLight.position.set(1, -0.5, 1)
 scene.add(pointLight)
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 6, 1, 1)
+rectAreaLight.position.set(-1.5, 0, 1.5)
+rectAreaLight.lookAt(new THREE.Vector3())
+scene.add(rectAreaLight)
+
+const spotLight = new THREE.SpotLight(0x78ff00, 4.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+scene.add(spotLight)
+
+scene.add(spotLight.target)
+spotLight.target.position.x = -0.75
+
+// Light Helpers
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2) //prettier-ignore
+scene.add(directionalLightHelper)
+
+const  hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight , 0.2) //prettier-ignore
+scene.add(hemisphereLightHelper)
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2)
+scene.add(pointLightHelper)
+
+const rectAreaLightHelper = new RectAreaLightHelper(rectAreaLight)
+scene.add(rectAreaLightHelper)
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight)
+scene.add(spotLightHelper)
 
 // Material
 const material = new THREE.MeshStandardMaterial()
@@ -31,7 +65,7 @@ sphere.position.x = -1.5
 
 const cube = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material)
 
-const torus = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 32, 64), material)
+const torus = new THREE.Mesh(new THREE.TorusGeometry(0.3, 0.2, 32, 64), material) //prettier-ignore
 torus.position.x = 1.5
 
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material)

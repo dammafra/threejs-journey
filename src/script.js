93ad7edb077d09a6e4a98839f45b1437898d203a
Francesco Dammacco <francescodammacco@live.it>
2025-01-20 23:43:12 +0100

    Starter

---
+++ src/script.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 3
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Animate
+const animate = () => {
+  // Update controls
+  controls.update()
+
+  renderer.render(scene, camera)
+}
+renderer.setAnimationLoop(animate)

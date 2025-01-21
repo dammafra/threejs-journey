ffeb90c86e1d331b1321906748536d7ad45cda19
Francesco Dammacco <francescodammacco@live.it>
2025-01-21 10:55:20 +0100

    Add shadows

--- src/script.js
+++ src/script.js
@@ -23,6 +23,10 @@ gltfLoader.setDRACOLoader(dracoLoader)
 let mixer = null
 
 gltfLoader.load('/models/hamburger.glb', gltf => {
+  gltf.scene.children.forEach(mesh => {
+    mesh.castShadow = true
+    mesh.receiveShadow = true
+  })
   scene.add(gltf.scene)
 })
 
@@ -46,7 +50,7 @@ scene.add(ambientLight)
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
 directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.far = 20
 directionalLight.shadow.camera.left = -7
 directionalLight.shadow.camera.top = 7
 directionalLight.shadow.camera.right = 7

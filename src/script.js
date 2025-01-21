c3554d802d776fe0617cc9a8e7fd24cd92bddb65
Francesco Dammacco <francescodammacco@live.it>
2025-01-21 11:10:34 +0100

    GLSL plugin, extensions and formatter setup

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 import GUI from 'lil-gui'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import testFragmentShader from './shaders/test/fragment.glsl'
+import testVertexShader from './shaders/test/vertex.glsl'
 
 // Debug
 const gui = new GUI()
@@ -18,7 +20,10 @@ const textureLoader = new THREE.TextureLoader()
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
 // Material
-const material = new THREE.MeshBasicMaterial()
+const material = new THREE.RawShaderMaterial({
+  vertexShader: testVertexShader,
+  fragmentShader: testFragmentShader,
+})
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)

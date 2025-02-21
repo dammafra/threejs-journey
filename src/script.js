edd21bf67dde6a22f1c226af0c0a385b072cead8
Francesco Dammacco <francescodammacco@live.it>
2025-02-21 17:47:19 +0100

    Add fullscreen support

--- src/script.js
+++ src/script.js
@@ -3,9 +3,36 @@ import Experience from './experience/experience'
 Experience.init(document.querySelector('canvas.webgl'))
 document.querySelectorAll('.fab').forEach(b => b.addEventListener('focus', e => e.target.blur()))
 
+const fullScreenButton = document.getElementById('fullscreen')
+fullScreenButton.addEventListener('click', () => {
+  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+  if (!fullscreenElement) {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen()
+    } else if (canvas.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen()
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen()
+    }
+  }
+})
+
 const isStandalone = () =>
   window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone
 
 if (isStandalone()) {
   document.body.classList.add('standalone')
+  fullScreenButton.remove()
+}
+
+const hasFullScreen = () =>
+  document.documentElement.requestFullscreen || document.documentElement.webkitRequestFullscreen
+
+if (!hasFullScreen()) {
+  fullScreenButton.remove()
 }

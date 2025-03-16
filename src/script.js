8e7e58a0d4e35c741bd5510139323287f04414cd
Francesco Dammacco <francescodammacco@live.it>
2025-03-16 15:33:09 +0100

    Spherical distribution

--- src/script.js
+++ src/script.js
@@ -69,7 +69,7 @@ const textures = [
   textureLoader.load('./particles/8.png'),
 ]
 
-const createFirework = ({ count, position, size, texture }) => {
+const createFirework = ({ radius, count, position, size, texture }) => {
   texture.flipY = false
 
   // Geometry
@@ -79,9 +79,18 @@ const createFirework = ({ count, position, size, texture }) => {
   for (let i = 0; i < count; i++) {
     const i3 = i * 3
 
-    positionsArray[i3 + 0] = Math.random() - 0.5
-    positionsArray[i3 + 1] = Math.random() - 0.5
-    positionsArray[i3 + 2] = Math.random() - 0.5
+    const spherical = new THREE.Spherical(
+      radius * (0.75 + Math.random() * 0.25),
+      Math.random() * Math.PI,
+      Math.random() * Math.PI * 2,
+    )
+
+    const position = new THREE.Vector3()
+    position.setFromSpherical(spherical)
+
+    positionsArray[i3 + 0] = position.x
+    positionsArray[i3 + 1] = position.y
+    positionsArray[i3 + 2] = position.z
 
     sizesArray[i] = Math.random()
   }
@@ -112,7 +121,13 @@ const createFirework = ({ count, position, size, texture }) => {
   scene.add(firework)
 }
 
-createFirework({ count: 100, position: new THREE.Vector3(), size: 0.5, texture: textures[7] })
+createFirework({
+  radius: 1,
+  count: 100,
+  position: new THREE.Vector3(),
+  size: 0.5,
+  texture: textures[7],
+})
 
 // Animate
 const tick = () => {

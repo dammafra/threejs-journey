269d0352bb70c01f67c86bf0e119b03d09be27b8
Francesco Dammacco <francescodammacco@live.it>
2025-03-16 23:15:49 +0100

    Ambient light

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,11 +1,20 @@
 uniform vec3 uColor;
 
-void main()
-{
-    vec3 color = uColor;
+#include '../includes/ambient-light.glsl'
 
-    // Final color
-    gl_FragColor = vec4(color, 1.0);
-    #include <tonemapping_fragment>
-    #include <colorspace_fragment>
+void main() {
+  vec3 color = uColor;
+
+  // Light
+  vec3 light = vec3(0.0);
+  light += ambientLight(vec3(1.0), 0.03);
+  color *= light;
+
+  // Final color
+  gl_FragColor = vec4(color, 1.0);
+
+  // clang-format off
+  #include <tonemapping_fragment>
+  #include <colorspace_fragment>
+  // clang-format on
 }

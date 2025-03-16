f4d0114009d6978f41f53fddb5660dd66da83ad7
Francesco Dammacco <francescodammacco@live.it>
2025-03-16 15:41:50 +0100

    Dispose

--- src/script.js
+++ src/script.js
@@ -123,8 +123,20 @@ const createFirework = ({ radius, count, position, size, texture, color }) => {
   firework.position.copy(position)
   scene.add(firework)
 
+  // Destroy
+  const destroy = () => {
+    scene.remove(firework)
+    geometry.dispose()
+    material.dispose()
+  }
+
   // Animate
-  gsap.to(material.uniforms.uProgress, { value: 1, duration: 3, ease: 'linear' })
+  gsap.to(material.uniforms.uProgress, {
+    value: 1,
+    duration: 3,
+    ease: 'linear',
+    onComplete: destroy,
+  })
 }
 
 createFirework({

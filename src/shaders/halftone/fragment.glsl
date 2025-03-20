e3c069b58b9eb6df9fe27e355975ffe7b2ba5b99
Francesco Dammacco <francescodammacco@live.it>
2025-03-20 22:21:27 +0100

    Lighting

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -3,10 +3,32 @@ uniform vec3 uColor;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
+#include '../includes/ambient-light.glsl'
+#include '../includes/directional-light.glsl'
+
 void main() {
   vec3 viewDirection = normalize(vPosition - cameraPosition);
   vec3 normal = normalize(vNormal);
   vec3 color = uColor;
+
+  // Light
+  vec3 light = vec3(0.0);
+
+  light += ambientLight( //
+      vec3(1.0),         // color
+      1.0                // intensity
+  );
+
+  light += directionalLight( //
+      vec3(1.0),             // color
+      1.0,                   // intensity
+      1.0,                   // specular power
+      vec3(1.0, 1.0, 0.0),   // position
+      normal,                //
+      viewDirection          //
+  );
+
+  color *= light;
 
   // Final color
   gl_FragColor = vec4(color, 1.0);

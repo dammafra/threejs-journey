90624aeaae53cf93da6e6ec9e3cb8dce2b8e3ee5
Francesco Dammacco <francescodammacco@live.it>
2025-03-22 16:00:16 +0100

    Radius and intensity

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -33,13 +33,19 @@ void main() {
 
   // Halftone
   float repetitions = 50.0;
+  vec3 direction = vec3(0.0, -1.0, 0.0);
+  float low = -0.8;
+  float high = 1.5;
+
+  float intensity = dot(normal, direction);
+  intensity = smoothstep(low, high, intensity);
 
   vec2 uv = gl_FragCoord.xy / uResolution.y;
   uv *= repetitions;
   uv = mod(uv, 1.0);
 
   float point = distance(uv, vec2(0.5));
-  point = step(0.5, point);
+  point = step(0.5 * intensity, point);
   point = 1.0 - point;
 
   // Final color

fdcdba8ea70951895a355f1cc06e31cb0219ba43
Francesco Dammacco <francescodammacco@live.it>
2025-06-12 23:48:00 +0200

    Basic wobble

--- src/shaders/wobble/vertex.glsl
+++ src/shaders/wobble/vertex.glsl
@@ -1,1 +1,11 @@
-void main() {}
+#include '../includes/simplexNoise4d.glsl'
+
+void main() {
+  // Wobble
+  float wobble = simplexNoise4d(vec4( //
+      csm_Position,                   // XYZ
+      0.0                             // W
+      ));
+
+  csm_Position += wobble * normal;
+}

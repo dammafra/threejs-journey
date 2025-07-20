012987df0c9fbc85f55fb5842a15781e3c1c6892
Francesco Dammacco <francescodammacco@live.it>
2025-07-21 00:06:45 +0200

    Starter

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client'
+import './style.css'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(<>Soon to be a badass R3F application</>)

aac5f876f858ee068bd75eae1f6717e6af7838c1
Francesco Dammacco <francescodammacco@live.it>
2025-07-21 18:27:31 +0200

    Lights

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -19,19 +19,22 @@ export default function Experience() {
     <>
       <orbitControls args={[camera, gl.domElement]} />
 
+      <directionalLight intensity={4.5} position={[1, 2, 3]} />
+      <ambientLight intensity={1.5} />
+
       <group ref={groupRef}>
         <mesh position-x={-2}>
           <sphereGeometry />
-          <meshBasicMaterial color="orange" />
+          <meshStandardMaterial color="orange" />
         </mesh>
         <mesh ref={cubeRef} position-x={2} rotation-y={Math.PI * 0.25} scale={1.5}>
           <boxGeometry />
-          <meshBasicMaterial color="mediumpurple" />
+          <meshStandardMaterial color="mediumpurple" />
         </mesh>
       </group>
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshBasicMaterial color="greenyellow" />
+        <meshStandardMaterial color="greenyellow" />
       </mesh>
     </>
   )

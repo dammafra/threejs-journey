6f2a040df1b03b2f48540b8b016328b5e5568de3
Francesco Dammacco <francescodammacco@live.it>
2025-07-21 18:16:07 +0200

    Groups

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,21 +3,25 @@ import { useRef } from 'react'
 
 export default function Experience() {
   const cubeRef = useRef(null)
+  const groupRef = useRef(null)
 
   useFrame((state, delta) => {
     cubeRef.current.rotation.y += delta
+    // groupRef.current.rotation.y += delta
   })
 
   return (
     <>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshBasicMaterial color="orange" />
-      </mesh>
-      <mesh ref={cubeRef} position-x={2} rotation-y={Math.PI * 0.25} scale={1.5}>
-        <boxGeometry />
-        <meshBasicMaterial color="mediumpurple" />
-      </mesh>
+      <group ref={groupRef}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshBasicMaterial color="orange" />
+        </mesh>
+        <mesh ref={cubeRef} position-x={2} rotation-y={Math.PI * 0.25} scale={1.5}>
+          <boxGeometry />
+          <meshBasicMaterial color="mediumpurple" />
+        </mesh>
+      </group>
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
         <meshBasicMaterial color="greenyellow" />

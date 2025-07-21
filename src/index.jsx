64b56056b926e99be3b2d3c3adbff5e1726ffd64
Francesco Dammacco <francescodammacco@live.it>
2025-07-21 19:10:24 +0200

    Final - Canvas settings

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,29 @@
 import { Canvas } from '@react-three/fiber'
 import ReactDOM from 'react-dom/client'
+import { ACESFilmicToneMapping, SRGBColorSpace } from 'three'
 import Experience from './Experience'
 import './style.css'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
-root.render(
-  <Canvas>
+const cameraSettings = root.render(
+  <Canvas
+    // dpr={[1, 2]} // default one
+    // flat
+    gl={{
+      antialias: true,
+      toneMapping: ACESFilmicToneMapping, // defualt one
+      outputColorSpace: SRGBColorSpace, // default one
+    }}
+    // orthographic
+    camera={{
+      fov: 45,
+      // zoom: 100,
+      near: 0.1,
+      far: 200,
+      position: [3, 2, 6],
+    }}
+  >
     <Experience />
   </Canvas>,
 )

c398c0bd71ea5a717535b94a04228ca530e0c4a8
Francesco Dammacco <francescodammacco@live.it>
2025-07-23 01:51:49 +0200

    StrictMode

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import { Canvas } from '@react-three/fiber'
+import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import Experience from './Experience.jsx'
 import './style.css'
@@ -6,14 +7,16 @@ import './style.css'
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-  <Canvas
-    camera={{
-      fov: 45,
-      near: 0.1,
-      far: 200,
-      position: [-4, 3, 6],
-    }}
-  >
-    <Experience />
-  </Canvas>,
+  <StrictMode>
+    <Canvas
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
+    >
+      <Experience />
+    </Canvas>
+  </StrictMode>,
 )

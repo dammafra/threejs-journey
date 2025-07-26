121db70780c5284d4919cd99b3719fc0d795f8c9
Francesco Dammacco <francescodammacco@live.it>
2025-07-26 13:39:36 +0200

    GLTF Loading with drei

--- src/Model.jsx
+++ src/Model.jsx
@@ -1,13 +1,8 @@
-import { useLoader } from '@react-three/fiber'
-import { DRACOLoader, GLTFLoader } from 'three/examples/jsm/Addons.js'
+import { useGLTF } from '@react-three/drei'
+
+useGLTF.preload('./models/hamburger-draco.glb')
 
 export default function Model() {
-  // prettier-ignore
-  const model = useLoader(
-    GLTFLoader, 
-    './models/FlightHelmet/glTF/FlightHelmet.gltf', 
-    loader => loader.setDRACOLoader(new DRACOLoader().setDecoderPath('./draco/'))
-  )
-
-  return <primitive object={model.scene} scale={5} position-y={-1} />
+  const model = useGLTF('./models/hamburger-draco.glb')
+  return <primitive object={model.scene} scale={0.35} />
 }

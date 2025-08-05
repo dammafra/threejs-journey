fe1f3b030663a1d22532d72f9e43dcb3080fb97e
Francesco Dammacco <francescodammacco@live.it>
2025-08-05 23:52:00 +0200

    Apply forces

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,12 +1,23 @@
 import { OrbitControls } from '@react-three/drei'
-import { BallCollider, CuboidCollider, Physics, RigidBody } from '@react-three/rapier'
+import { Physics, RigidBody } from '@react-three/rapier'
 import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
+import { useRef } from 'react'
+import { Vector3 } from 'three'
 
 export default function Experience() {
+  const cube = useRef()
+
   const { debug } = useControls({
     debug: process.env.NODE_ENV === 'development' || window.location.hash.includes('#debug'),
   })
+
+  const cubeJump = () => {
+    cube.current.applyImpulse(new Vector3(0, 5, 0))
+    cube.current.applyTorqueImpulse(
+      new Vector3(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5),
+    )
+  }
 
   return (
     <>
@@ -19,22 +30,15 @@ export default function Experience() {
 
       <Physics debug={debug}>
         <RigidBody colliders="ball">
-          <mesh castShadow position={[0, 4, 0]}>
+          <mesh castShadow position={[-1.5, 2, 0]}>
             <sphereGeometry />
             <meshStandardMaterial color="orange" />
           </mesh>
         </RigidBody>
 
-        <RigidBody colliders={false} position={[0, 1, -0.25]} rotation-x={Math.PI * 0.1}>
-          <CuboidCollider args={[1.5, 1.5, 0.5]} />
-          <CuboidCollider
-            args={[0.25, 1, 0.25]}
-            position={[0, 0, 1]}
-            rotation={[-Math.PI * 0.35, 0, 0]}
-          />
-          <BallCollider args={[1.5]} />
-          <mesh castShadow>
-            <torusGeometry args={[1, 0.5, 16, 32]} />
+        <RigidBody ref={cube}>
+          <mesh castShadow position={[1.5, 2, 0]} onClick={cubeJump}>
+            <boxGeometry />
             <meshStandardMaterial color="mediumpurple" />
           </mesh>
         </RigidBody>

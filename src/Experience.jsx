c1ac3f7ab27bd6c73b1338313ddf7e00869085a1
Francesco Dammacco <francescodammacco@live.it>
2025-08-05 23:30:05 +0200

    Colliders

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,7 +1,13 @@
 import { OrbitControls } from '@react-three/drei'
+import { Physics, RigidBody } from '@react-three/rapier'
+import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
 
 export default function Experience() {
+  const { debug } = useControls({
+    debug: process.env.NODE_ENV === 'development' || window.location.hash.includes('#debug'),
+  })
+
   return (
     <>
       <Perf position="top-left" />
@@ -11,20 +17,39 @@ export default function Experience() {
       <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
       <ambientLight intensity={1.5} />
 
-      <mesh castShadow position={[-2, 2, 0]}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+      <Physics debug={debug}>
+        <RigidBody colliders="ball">
+          <mesh castShadow position={[-2, 4, 0]}>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+          </mesh>
+        </RigidBody>
 
-      <mesh castShadow position={[2, 2, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh>
+        <RigidBody>
+          <mesh castShadow position={[2, 2, 0]}>
+            <boxGeometry args={[2, 2, 1]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+          <mesh castShadow position={[2, 2, 3]}>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody>
 
-      <mesh receiveShadow position-y={-1.25}>
-        <boxGeometry args={[10, 0.5, 10]} />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
+        <RigidBody colliders="trimesh">
+          <mesh castShadow position={[-2, 1, -0.25]} rotation-x={Math.PI * 0.1}>
+            <torusGeometry args={[1, 0.5, 16, 32]} />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </RigidBody>
+
+        <RigidBody type="fixed">
+          <mesh receiveShadow position-y={-1.25}>
+            <boxGeometry args={[10, 0.5, 10]} />
+            <meshStandardMaterial color="greenyellow" />
+          </mesh>
+        </RigidBody>
+      </Physics>
     </>
   )
 }
